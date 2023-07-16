1fc5d825d0dc69eccf9b40734225c421a93dd580
Karin Prater <gahntpo@yahoo.de>
2023-07-16 11:49:05 +0300

    Initial Commit

---
+++ BookstoreStategist/BookstoreStategistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookstoreStategistApp.swift
+//  BookstoreStategist
+//
+//  Created by Karin Prater on 16.07.23.
+//
+
+import SwiftUI
+
+@main
+struct BookstoreStategistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookstoreStategist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookstoreStategist
+//
+//  Created by Karin Prater on 16.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
